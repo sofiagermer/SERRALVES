86ec0c8b7d8647e9e08785fdddc3821cae77e132
MarcioMadeira773 <39671616+MarcioMadeira773@users.noreply.github.com>
2021-05-21 12:05:56 +0100

    Update int3.sql

--- PART 3/queries/int3.sql
+++ PART 3/queries/int3.sql
@@ -1,6 +1,9 @@
 /*
-Interrogação à base de dados que mostra o nome, o NIF respetivo e o salário em euros/mes dos trabalhadores de Serralves por ordem descendente.
+Interrogação à base de dados que mostra o nome, o NIF respetivo e o salário em euros/mes dos trabalhadores de Serralves por ordem decrescente.
 */
+
+.mode columns
+.headers on
 
 SELECT Pessoa.Nome, Pessoa.NIF, Staff.Salario "Salario (euros/mes)"
 FROM Pessoa
