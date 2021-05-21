32cf3ba3e1d3962c4ca1dffd15731e0efad7c3b2
MarcioMadeira773 <39671616+MarcioMadeira773@users.noreply.github.com>
2021-05-21 12:06:14 +0100

    Update int4.sql

--- PART 3/queries/int4.sql
+++ PART 3/queries/int4.sql
@@ -2,6 +2,9 @@
 Interrogação à base de dados que mostra o nome do artista e o título das obras que estão expostas na exposição premanente de Serralves.
 */
 
+.mode columns
+.headers on
+
 SELECT Artista.Nome, Obra.Nome, Exposicao.Nome
 From Obra
 INNER JOIN ObraArtista ON Obra.IDObra = ObraArtista.IDObra
