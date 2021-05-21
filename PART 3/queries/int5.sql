3f5ee6fb9f322b8df1407dd8d7cba645787a9016
MarcioMadeira773 <39671616+MarcioMadeira773@users.noreply.github.com>
2021-05-21 12:06:30 +0100

    Update int5.sql

--- PART 3/queries/int5.sql
+++ PART 3/queries/int5.sql
@@ -2,6 +2,9 @@
 Interrogação à base de dados que mostra todos os turnos, com a hora de entrade e a hora de saída, e quantos trablhadores estão alocados a cada turno.
 */
 
+.mode columns
+.headers on
+
 SELECT Horario.HoraEntrada, Horario.HoraSaida, count(*) "Numero de trabalhadores neste horario"
 FROM HorarioStaff
 INNER JOIN Horario ON Horario.IDHorario = HorarioStaff.IDHorario
