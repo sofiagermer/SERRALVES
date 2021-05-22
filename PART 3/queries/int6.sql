fb0b7e56bc466a01d3e94eb1986dbc98d80cf96d
sofiagermer <sofiagermer8@gmail.com>
2021-05-22 12:39:32 +0100

    Added more Horarios for DiversityA

---
+++ PART 3/queries/int6.sql
@@ -0,0 +1,9 @@
+.mode columns
+.headers on
+.nullvalue NULL 
+
+
+select NIF, Salario, (sum(abs(strftime('%s', HoraSaida) - strftime('%s', HoraEntrada)))) / 3600 as HorasSemanais
+from   (Staff join Horario) natural join Horario natural join Staff
+group by NIF
+order by HorasSemanais DESC;
