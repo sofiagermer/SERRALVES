3b76530f3d97e4dc254fa4e5afc5437b065e4859
Miguel Lopes <lopesazevedom@gmail.com>
2021-05-23 00:41:18 +0100

    all triggers are pretty now

--- PART 3/triggers/trigger2_check.sql
+++ PART 3/triggers/trigger2_check.sql
@@ -4,10 +4,34 @@
 
 PRAGMA foreign_keys = ON;
 
+.print ''
+.print 'Vamos utilizar o Curador como teste.'
+.print 'Atualmente o horario e salario dele são os seguintes:'
+.print ''
+
+
+SELECT Salario,HoraEntrada,HoraSaida
+FROM Staff JOIN (SELECT NIF,HoraEntrada,HoraSaida 
+FROM HorarioStaff JOIN Horario USING(IDHorario)) USING(NIF)
+WHERE (NIF=245684123);
+
+.print ''
+.print 'Como se pode observar atualmente o Curador trabalha 6 horas.'
+.print 'Vamos mudar o seu horario de forma a que passe a trabalhar 9 horas'
+.print ''
 
 UPDATE HorarioStaff 
 SET IDHorario = 2
 WHERE (NIF=245684123);
 
-SELECT Salario FROM Staff WHERE NIF=245684123;
+SELECT Salario,HoraEntrada,HoraSaida
+FROM Staff JOIN (SELECT NIF,HoraEntrada,HoraSaida 
+FROM HorarioStaff JOIN Horario USING(IDHorario)) USING(NIF)
+WHERE (NIF=245684123);
+
+
+.print ''
+.print 'A mudança refletiu-se proporcionalmente no salário que aumentou para 12000'
+.print ''
+
 
--- PART 3/triggers/trigger3_check.sql
+++ PART 3/triggers/trigger3_check.sql
@@ -5,38 +5,51 @@ PRAGMA foreign_keys = ON;
 
 DELETE FROM Vigilante WHERE IDObra = 12158641;
 
+.print ''
 .print 'Fazemos DELETE a um valor da tabela Vigilante para termos uma obra onde fazer o teste'
+.print ''
 
 SELECT *
 FROM Vigilante
 WHERE IDObra = 12158641;
 
+.print ''
 .print 'Verifica-se que já não está na tabela Vigilante e então a obra não está a ser vigiada'
+.print ''
 
 SELECT * 
 FROM Seguranca
 WHERE NIF = 121542324;
 
+.print ''
 .print 'O membro do staff com o NIF: 121542324 já é um Segurança de Serralves'
+.print ''
 
 INSERT INTO Vigilante VALUES (121542324, 12158641);
 
+.print ''
 .print 'Ao tentar inserir na tabela Vigilante um NIF que pertence a um Segurança, aparece um erro que um membro do staff que já é Segurança não pode ser um Vigilante em simultaneo.'
+.print ''
 
 INSERT INTO Vigilante VALUES (879367829, 12158641);
 
+.print ''
 .print 'Inserimos a linha que retiramos para fazer o teste para manter intacta a base de dados.'
-
+.print ''
 
 SELECT *
 FROM Vigilante
 WHERE NIF = 101029353;
 
+.print ''
 .print 'Verifica-se que o membro do staff com NIF: 101029353 é Vigilante.'
+.print ''
 
 INSERT INTO Seguranca VALUES (101029353,1);
 
+.print ''
 .print 'Ao inserir na tabela Seguranca um NIF que já está presente na tabela Vigilante, aparece um erro que um membro do staff que já é Vigilante não pode ser um Segurança em simultaneo.'
+.print ''
 
 SELECT *
 FROM Seguranca;
