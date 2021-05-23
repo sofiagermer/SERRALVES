d33b06a5aec455f329a87cf6c102eaf1e34a0ba0
sofiagermer <sofiagermer8@gmail.com>
2021-05-23 01:43:20 +0100

    Estamos Quase Miguelito

--- PART 3/queries/int3.sql
+++ PART 3/queries/int3.sql
@@ -9,7 +9,10 @@
 
 SELECT Pessoa.Nome, Pessoa.NIF, Staff.Salario "Salario (euros/mes)", (HoraSaida- HoraEntrada)* 5 AS HorasSemanais
 FROM Pessoa
-JOIN HorarioStaff USING (NIF)
-JOIN Horario ON HorarioStaff.IDHorario = Horario.IDHorario
-JOIN Staff USING(NIF)
+JOIN HorarioStaff 
+    USING (NIF)
+JOIN Horario 
+    ON HorarioStaff.IDHorario = Horario.IDHorario
+JOIN Staff 
+    USING(NIF)
 ORDER BY Staff.Salario DESC;
